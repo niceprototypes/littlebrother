41d5b8ecc0609d5bd0c395612b6b03b0899ee34d
Mohammed Ibrahim <mohammedibrahim@MacBook-Pro-99.attlocal.net>
2023-10-19 22:59:19 -0700

    Replaced react-ellispsis with custom component

--- src/components/Ellipsis.js
+++ src/components/Ellipsis.js
@@ -3,28 +3,33 @@ import React from "react"
 import LinesEllipsis from "react-lines-ellipsis"
 import Text from "./Text"
 
-const Ellipsis = ({isTrimmed, message, onReflow}) => {
-  return isTrimmed ? (
-    <LinesEllipsis
-      basedOn="words"
-      component="span"
-      ellipsis={
-        <>
-          ...{" "}
-          <Text color="link.primary" fontSize="p2" fontWeight="bold">
-            more
-          </Text>
-        </>
-      }
-      maxLine="1"
-      onReflow={onReflow}
-      style={{display: "inline"}}
-      text={message}
-      trimRight
-    />
-  ) : (
+const Ellipsis = ({isTrimmed, message}) => {
+  if (message.length < 20) {
+    return (
+      <Text fontSize="p2" fontWeight="bold">
+        {message}
+      </Text>
+    )
+  }
+
+  if (isTrimmed) {
+    return (
+      <>
+        <Text fontSize="p2" fontWeight="bold">
+          {message.substring(0, 95)}...{" "}
+        </Text>
+        <Text color="link.primary" fontSize="p2" fontWeight="bold">
+          more
+        </Text>
+      </>
+    )
+  }
+
+  return (
     <>
-      {message}{" "}
+      <Text fontSize="p2" fontWeight="bold">
+        {message}
+      </Text>
       <Text color="link.primary" fontSize="p2" fontWeight="bold">
         less
       </Text>
